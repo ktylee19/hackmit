27ae6d1dc49dc7d05a06dd5b5a9930165b1fec8b
Katie Lee <ktylee19@gmail.com>
2014-10-05 08:40:14 -0400

    Accelerometer data to move player around map

--- client/views/play/play.js
+++ client/views/play/play.js
@@ -20,72 +20,50 @@ Template.play.rendered = function(e) {
 }
 
 Template.play.events({
-    'click #right': function() {
-        $('img').css("left", function() {
-            var left = parseInt($('img').css("left"));
-            if (left < -1800)
-                return left;
-            else
-                return left - 100;
-        });
-        $('#goal').css("left", function() {
-            var left = parseInt($('#goal').css("left"));
-            if (left < -1800)
-                return left;
-            else
-                return left - 100;
-        });
-    },
-    'click #up': function() {
-        $('img').css("top", function() {
-            var top = parseInt($('img').css("top"));
-            if (top > -100)
-                return top;
-            else
-                return top + 100;
-        });
-        $('#goal').css("top", function() {
-            var top = parseInt($('#goal').css("top"));
-            if (top > -100)
-                return top;
-            else
-                return top + 100;
-        });
-    },
-    'click #down': function() {
-        $('img').css("top", function() {
-            var up = parseInt($('img').css("top"));
-            if (up < -2400)
-                return up;
-            else
-                return up - 100;
-        });
-        $('#goal').css("top", function() {
-            var top = parseInt($('#goal').css("top"));
-            if (top < -2400)
-                return top;
-            else
-                return top - 100;
-        });
-    },
-    'click #left': function() {
-        $('img').css("left", function() {
-            var right = parseInt($('img').css("left"));
-            if (right > -100)
-                return right;
-            else
-                return right + 100;
-        });
-        $('#goal').css("left", function() {
-            var top = parseInt($('#goal').css("left"));
-            if (top > -100)
-                return top;
-            else
-                return top + 100;
-        });
-    },
     'click #goal': function() {
         winner = true;
         Router.go("finish");
     }
+});
+
+// event listeners for acceleration
+window.addEventListener("deviceorientation", function(event) {
+    // alpha: rotation around z-axis
+    var rotateDegrees = event.alpha;
+    // gamma: left to right
+    var leftToRight = event.gamma;
+    // beta: front back motion
+    var frontToBack = event.beta;
+
+    // left-right motion
+    $('img').css("left", function() {
+        var angle = parseInt($('img').css("left"));
+        if (Math.abs(leftToRight) > 20)
+            return angle - leftToRight;
+        else
+            return angle;
+    });
+    $('#goal').css("left", function() {
+        var angle = parseInt($('img').css("left"));
+        if (Math.abs(leftToRight) > 20)
+            return angle - leftToRight;
+        else
+            return angle;
+    });
+
+    // front-back motion
+    $('img').css("top", function() {
+        var angle = parseInt($('img').css("top"));
+        if (Math.abs(frontToBack) > 20)
+            return angle - frontToBack;
+        else
+            return angle;
+        });
+    $('#goal').css("top", function() {
+        var angle = parseInt($('img').css("top"));
+        if (Math.abs(frontToBack) > 20)
+            return angle - frontToBack;
+        else
+            return angle;
+    });
 });
